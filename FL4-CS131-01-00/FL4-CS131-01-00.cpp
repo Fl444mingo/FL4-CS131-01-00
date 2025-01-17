e8c40ea9dfdc36dc57821c3b162baf6becf28e3c
thesp <thesp@DESKTOP-U53I3V6>
2025-01-16 22:46:54 -0800

    Add project files.

---
+++ FL4-CS131-01-00/FL4-CS131-01-00.cpp
@@ -0,0 +1,18 @@
+// Written with guidance code from chapter 2 of "Fundamentals of C++ Programming" by Richard L. Halterman
+// Comments from me as notes of reference
+
+#include <iostream>
+
+// Statement name simplifications:
+// using std::cout;
+// using namespace std;
+
+int main() {
+	// Simplified statement:
+	// cout << "Console message\n";
+	std::cout << "   *    *   \n"
+			  << "   *    *   \n"
+			  << "            \n"
+			  << " *        * \n"
+			  << "   ******   \n";
+}
